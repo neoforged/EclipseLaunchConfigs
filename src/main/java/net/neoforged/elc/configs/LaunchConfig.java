7d56bdd764234181ab06df01d38eca0dee3e93a5
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2023-09-25 01:35:48 -0700

    Java App config

---
+++ src/main/java/net/neoforged/elc/configs/LaunchConfig.java
@@ -0,0 +1,48 @@
+package net.neoforged.elc.configs;
+
+import java.io.Writer;
+import java.util.List;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import net.neoforged.elc.attributes.EAttribute;
+
+/**
+ * Base interface representing any Eclipse launch configuration.
+ */
+public interface LaunchConfig {
+
+    /**
+     * Returns the type name that corresponds to this launch config.
+     */
+    String getType();
+
+    /**
+     * Serializes this launch configuration into E-Attribute form for {@link #write(Writer)}.
+     * 
+     * @return A list containing all E-Attributes that make up this launch config.
+     */
+    List<EAttribute> bakeAttributes();
+
+    /**
+     * Writes this launch config as XML to the given output stream.
+     * 
+     * @param outputStream The target output stream.
+     */
+    default void write(Writer outputStream) throws XMLStreamException {
+        final XMLOutputFactory outputFactory = XMLOutputFactory.newFactory();
+        final XMLStreamWriter writer = outputFactory.createXMLStreamWriter(outputStream);
+        writer.writeStartDocument();
+
+        writer.writeStartElement("launchConfiguration");
+        writer.writeAttribute("type", getType());
+        for (EAttribute attribute : bakeAttributes()) {
+            attribute.write(writer, outputFactory);
+        }
+
+        writer.writeEndElement();
+        writer.writeEndDocument();
+    }
+}
